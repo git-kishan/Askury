9d671f56160ba94bc28579a1388f57cd584720d4
kishan kumar <kishankumar001122@gmail.com>
2019-05-29 12:38:51 +0530

    1.0.6

--- app/src/main/java/com/droid/solver/askapp/Main/MainActivity.java
+++ app/src/main/java/com/droid/solver/askapp/Main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.droid.solver.askapp.Main;
 
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -9,7 +11,9 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.droid.solver.askapp.Account.AccountFragment;
 import com.droid.solver.askapp.Community.CommunityFragment;
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements
     private static final String COMMUNITY="community";
     private static final String ACCOUNT="ic_account";
     BottomNavigationView bottomNavigationView;
-    FrameLayout frameLayout,profileImageContainer;
+    FrameLayout frameLayout;
     Toolbar toolbar;
     private CardView toolbarCardView;
     @Override
@@ -36,10 +40,10 @@ public class MainActivity extends AppCompatActivity implements
         toolbarCardView=findViewById(R.id.toolbar_card_view);
         frameLayout=findViewById(R.id.fragment_container);
         bottomNavigationView=findViewById(R.id.bottom_navigation);
-        profileImageContainer=findViewById(R.id.profile_image_container);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         loadFragment(new HomeFragment(),HOME);
+        changeToolbarFont(toolbar, this);
 
     }
 
@@ -104,4 +108,21 @@ public class MainActivity extends AppCompatActivity implements
     }
 
 
+
+    public static void changeToolbarFont(Toolbar toolbar, Activity context) {
+        for (int i = 0; i < toolbar.getChildCount(); i++) {
+            View view = toolbar.getChildAt(i);
+            if (view instanceof TextView) {
+                TextView tv = (TextView) view;
+                if (tv.getText().equals(toolbar.getTitle())) {
+                    applyFont(tv, context);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void applyFont(TextView tv, Activity context) {
+        tv.setTypeface(Typeface.createFromAsset(context.getAssets(), "aclonica.ttf"));
+    }
 }
